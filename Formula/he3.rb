495a880e25aebff4a4a08dd52a22a6494850e09a
wangwei <434533508@qq.com>
2022-10-31 15:14:44 +0800

    feat: homebrew

---
+++ Formula/he3.rb
@@ -0,0 +1,14 @@
+class He3 < Formula
+    arch arm: "arm64", intel: "amd64"
+    version "0.0.15"
+    sha256 arm:   "27e37f60e760491655ac067ee0317b60e38fc1e2208baf396e1da7a74f9c0916",
+           intel: "703cf23a9574b9f81b7e1cd834f910625da690ec125d50e281204f13fbb35b09"
+    url "https://he3-1309519128.cos.accelerate.myqcloud.com/#{version}/He3_mac_#{arch}_#{version}.dmg"
+    name "He3"
+    desc "A Free, Modern Toolbox Built for Developers."
+    homepage "https://he3.app"
+
+    depends_on macos: ">= :high_sierra"
+
+    app "He3.app"
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# homebrew-he3
+
+HomeBrew repo for he3
+
+```
+brew install h8r-dev/repo/he3-client
+```
+
+See https://brew.sh/ for more details about HomeBrew.
