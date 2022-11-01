6f96bfdcbd0d00536e670ac9828e22ac4be2f4ff
lyzhang1999 <434533508@qq.com>
2022-11-01 02:20:53 +0000

    ${VERSION}

--- Casks/he3.rb
+++ Casks/he3.rb
@@ -1,26 +1,26 @@
 cask "he3" do
-    arch arm: "arm64", intel: "amd64"
-    version "0.0.15"
-    sha256 arm:   "27e37f60e760491655ac067ee0317b60e38fc1e2208baf396e1da7a74f9c0916",
-           intel: "703cf23a9574b9f81b7e1cd834f910625da690ec125d50e281204f13fbb35b09"
-           
-    url "https://he3-1309519128.cos.accelerate.myqcloud.com/#{version}/He3_mac_#{arch}_#{version}.dmg"
-    
-    livecheck do
-      url :url
-    end
+  arch arm: "arm64", intel: "amd64"
+  version "0.0.17"
+  sha256 arm:   "d39cd1e943f5d7c9db9ce9531ed7c347739b135fb791f91c7ad1e24d4c20c1bd",
+         intel: "d22895a814629fd94d330f99ce85a6eaa2fce5144ce3453ba1368a91e9cc5fde"
+          
+  url "https://he3-1309519128.cos.accelerate.myqcloud.com/#{version}/He3_mac_#{arch}_#{version}.dmg"
+  
+  livecheck do
+    url :url
+  end
 
-    auto_updates true
+  auto_updates true
 
-    name "He3"
-    desc "A Free, Modern Toolbox Built for Developers."
-    homepage "https://he3.app"
+  name "He3"
+  desc "A Free, Modern Toolbox Built for Developers."
+  homepage "https://he3.app"
 
-    depends_on macos: ">= :catalina"
+  depends_on macos: ">= :catalina"
 
-    app "He3.app"
+  app "He3.app"
 
-    zap trash: [
-      "~/Library/Application Support/he3"
-    ]
+  zap trash: [
+    "~/Library/Application Support/he3"
+  ]
 end
