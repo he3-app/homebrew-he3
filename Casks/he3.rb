e8fa183eefdc49d6c34546a46efb1c8b48f6f538
wangwei <lyzhang1999@users.noreply.github.com>
2022-11-12 09:23:39 +0800

    Update he3.rb

--- Casks/he3.rb
+++ Casks/he3.rb
@@ -1,5 +1,5 @@
 cask "he3" do
-  arch arm: "arm64", intel: "amd64"
+  arch arm: "arm64", intel: "x"
   version "1.0.2"
   sha256 arm:   "90fc0b550fcf1b8a109e23acdd5d5e0b974326aa51a381b6998f75e600e96847",
          intel: "cc2987bd390a09836d5152bcb5995b5cc0260974195fc3ac1f234e0d76afa7d2"
