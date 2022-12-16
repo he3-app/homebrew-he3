6d7b5c14c66c34f44f4a90977a88d7ce94c46249
lyzhang1999 <434533508@qq.com>
2022-12-16 05:04:32 +0000

    1.2.1

--- Casks/he3.rb
+++ Casks/he3.rb
@@ -1,8 +1,8 @@
 cask "he3" do
   arch arm: "arm64", intel: "x64"
-  version "1.1.1"
-  sha256 arm:   "c6d934e88933480fbff3b0188f56ec667b838abb474122c7f484668fba18d021",
-         intel: "d2582d239b255907420a01db13c2d934457a9f7c5260a057ba4a3467607e2631"
+  version "1.2.1"
+  sha256 arm:   "0c3b80097bd9bc113ac67c9aaaea10089e0a07f5e5c429fd0b7184f8bf1f47fe",
+         intel: "cce0657e75e1fff22bfb3543bd0f5570779a300960c3cea47977a8447fdee9b8"
           
   url "https://he3-1309519128.cos.accelerate.myqcloud.com/#{version}/He3_mac_#{arch}_#{version}.dmg"
   
